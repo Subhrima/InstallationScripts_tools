8ffbbba7023e89476b4dcf9e1b59fb9dd45076d0
Subhrima Raha <43644960+Subhrima@users.noreply.github.com>
2026-02-23 15:23:49 +0200

    Update docker.sh to fix apt-cache command
    Comment out the apt-cache policy command and its option for Ubuntu compatibility.

--- docker.sh
+++ docker.sh
@@ -10,7 +10,9 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 
 sudo apt update -y
 
-apt-cache policy docker-ce -y
+#apt-cache policy docker-ce -y 
+# -y doesnot work for ubuntu
+apt-cache policy docker-ce 
 
 sudo apt install docker-ce -y
 
